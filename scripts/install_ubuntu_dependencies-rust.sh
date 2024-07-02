bcc60de6cac2899d20deef7a0568908eed050dcd
C.Lee Taylor <leet@tari.com>
2024-07-02 10:37:35 +0200

    ci(fix): update scripts for ci workflows

---
+++ scripts/install_ubuntu_dependencies-rust.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+#
+# Install rust unattended - https://www.rust-lang.org/tools/install
+#
+
+set -e
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+. "$HOME/.cargo/env"
