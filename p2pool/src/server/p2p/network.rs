022b954a0656668bbdf538fdc598029aed1bfc9c
SW van Heerden <swvheerden@gmail.com>
2025-05-12 09:35:45 +0200

    clippy

--- p2pool/src/server/p2p/network.rs
+++ p2pool/src/server/p2p/network.rs
@@ -2210,6 +2210,7 @@ where S: ShareChain
         // }
     }
 
+    #[allow(clippy::too_many_lines)]
     async fn perform_catch_up_sync(&mut self, perform_catch_up_sync: PerformCatchUpSync) -> Result<(), Error> {
         let PerformCatchUpSync {
             algo,
